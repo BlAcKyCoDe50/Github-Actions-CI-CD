4159549cd2bd691a8e71aa5e02f6f43f5e51fb27
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-08-25 11:41:59 +0530

    added the src and test cases

---
+++ src/addition.py
@@ -0,0 +1,10 @@
+# testing the addtion function
+
+def add(num1 , num2):
+    return num1+num2
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(2,9) == 11
+    assert add(1,5) == 6 
+    assert add(5,5) == 10 
